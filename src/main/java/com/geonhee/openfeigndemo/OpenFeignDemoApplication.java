ff7c634519dd6e9400e120b533c7c5c87966f400
geonhee.sung <geonhee.sung@nhnsoft.com>
2022-09-15 18:25:09 +0900

    Initial commit

---
+++ src/main/java/com/geonhee/openfeigndemo/OpenFeignDemoApplication.java
@@ -0,0 +1,13 @@
+package com.geonhee.openfeigndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignDemoApplication.class, args);
+    }
+
+}
